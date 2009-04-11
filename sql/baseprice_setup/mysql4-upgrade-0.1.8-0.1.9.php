ebc4c9217c8272c2c985f4d48d1f570200b308ca
vinai <vinai@netzarbeiter.com>
2009-04-11 14:29:07 +0200

    didn't update the setup script!
    forgot to update the setup script

--- sql/baseprice_setup/mysql4-upgrade-0.1.8-0.1.9.php
+++ sql/baseprice_setup/mysql4-upgrade-0.1.8-0.1.9.php
@@ -1,13 +1,17 @@
 <?php
 
-$this->startSetup();
-$this->endSetup();
 
-try
+$this->startSetup();
+
+if (version_compare(Mage::getVersion(), '1.3.0', '>='))
 {
-    Mage::getResourceModel('catalog/product_flat_indexer')->rebuild();
+    /**
+     * We can't rebuild the flat catalog here - we cant even register messages in
+     * the session while we are in a setup script. So, this is a hack around this...
+     *
+     * @see DerModPro_BasePrice_Model_Observer::controllerActionLayoutLoadBefore()
+     */
+    Mage::register('baseprice_rebuild_flat_product_catalog', 1);
 }
-catch (Exception $e)
-{
-    //Mage::log("Error building flat product catalog: " . $e->getMessage());
-}
+
+$this->endSetup();
